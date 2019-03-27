0f6b4d4e089899626f7d68bda65cd09b66cf5cde
root705829 <3525088417@qq.com>
2019-03-28 01:04:28 +0800

    引入登录组建

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>login---</div>
+    <div class="login-wrap">
+        <h2>登录组件</h2>
+        <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+            <el-form-item label="用户名">
+              <el-input v-model="formdata.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input v-model="formdata.password"></el-input>
+            </el-form-item>
+        </el-form>
+        <el-button class="login-btn" type="success">成功按钮</el-button>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            formdata:{
+                username:'',
+                password:''
+            }
+        }
+    }
 }
 </script>
 
